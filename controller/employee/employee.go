110d7fd7afb1201adb9ad96e61f9f9ab346b8528
Khao Seekakun <emitstudio13829@gmail.com>
2024-11-29 11:20:49 +0700

    Update employee.go

--- controller/employee/employee.go
+++ controller/employee/employee.go
@@ -6,33 +6,54 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// GET Methord Main
+type employee struct {
+	Emp_id         string  `json:"emp_id"`
+	Emp_firstname  string  `json:"emp_firstname"`
+	Emp_lastname   string  `json:"emp_lastname"`
+	Emp_department string  `json:"emp_department"`
+	Emp_salary     float64 `json:"emp_salary"`
+}
+
+var employees = []employee{
+	{Emp_id: "1", Emp_firstname: "Blue", Emp_lastname: "Coltrane", Emp_department: "sales", Emp_salary: 50000.00},
+	{Emp_id: "2", Emp_firstname: "Jeru", Emp_lastname: "Mulligan", Emp_department: "marketing", Emp_salary: 55000.00},
+	{Emp_id: "3", Emp_firstname: "Sarah", Emp_lastname: "Vaughan", Emp_department: "IT", Emp_salary: 80000.00},
+}
+
 func GetMain(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
+		"status":  "ok",
 		"message": "Please use path /employee",
 	})
 }
 
-// GET Method
 func GetEmployee(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "GET METHOD EMPLOYEE",
-	})
+	c.IndentedJSON(http.StatusOK, employees)
 }
 
-// GET Method by ID
 func GetEmployeeByID(c *gin.Context) {
 	id := c.Param("id")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "GET METHOD EMPLOYEE BY ID",
-		"id":      id,
+
+	for _, emp := range employees {
+		if emp.Emp_id == id {
+			c.IndentedJSON(http.StatusOK, gin.H{
+				"data":   emp,
+				"status": "ok",
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{
+		"status":  "not_found",
+		"message": "Employee not found",
 	})
 }
 
-// POST Method
 func PostEmployee(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "POST METHOD EMPLOYEE",
+		"status":  "ok",
 	})
 }
 
@@ -40,6 +61,7 @@ func PostEmployee(c *gin.Context) {
 func PutEmployee(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "PUT METHOD EMPLOYEE",
+		"status":  "ok",
 	})
 }
 
@@ -47,5 +69,6 @@ func PutEmployee(c *gin.Context) {
 func DeleteEmployee(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "DELETE METHOD EMPLOYEE",
+		"status":  "ok",
 	})
 }
