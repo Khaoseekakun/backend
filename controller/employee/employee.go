2a557d5d415418a86ffca60b573692d507066816
Khao Seekakun <emitstudio13829@gmail.com>
2024-11-29 11:58:01 +0700

    ffff

--- controller/employee/employee.go
+++ controller/employee/employee.go
@@ -3,21 +3,15 @@ package employee
 import (
 	"net/http"
 
+	"backend/api/db"
+
 	"github.com/gin-gonic/gin"
 )
 
-type employee struct {
-	Emp_id         string  `json:"emp_id"`
-	Emp_firstname  string  `json:"emp_firstname"`
-	Emp_lastname   string  `json:"emp_lastname"`
-	Emp_department string  `json:"emp_department"`
-	Emp_salary     float64 `json:"emp_salary"`
-}
-
-var employees = []employee{
-	{Emp_id: "1", Emp_firstname: "Blue", Emp_lastname: "Coltrane", Emp_department: "sales", Emp_salary: 50000.00},
-	{Emp_id: "2", Emp_firstname: "Jeru", Emp_lastname: "Mulligan", Emp_department: "marketing", Emp_salary: 55000.00},
-	{Emp_id: "3", Emp_firstname: "Sarah", Emp_lastname: "Vaughan", Emp_department: "IT", Emp_salary: 80000.00},
+type Tbl_employee struct {
+	Emp_id        int
+	Emp_firstname string
+	Emp_lastname  string
 }
 
 func GetMain(c *gin.Context) {
@@ -27,30 +21,10 @@ func GetMain(c *gin.Context) {
 	})
 }
 
-func GetEmployee(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"data":   employees,
-		"status": "ok",
-	})
-}
-
-func GetEmployeeByID(c *gin.Context) {
-	id := c.Param("id")
-
-	for _, emp := range employees {
-		if emp.Emp_id == id {
-			c.IndentedJSON(http.StatusOK, gin.H{
-				"data":   emp,
-				"status": "ok",
-			})
-			return
-		}
-	}
-
-	c.JSON(http.StatusNotFound, gin.H{
-		"status":  "not_found",
-		"message": "Employee not found",
-	})
+func GetEmployeeDB(c *gin.Context) {
+	var employee []Tbl_employee
+	db.Db.Find(&employee)
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "message": "Employee Read Success", "employees": employee})
 }
 
 func PostEmployee(c *gin.Context) {
--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	EmployeeController "backend/api/controller/employee"
+	"fmt"
+
+	"backend/api/db"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	//get InitDB fuction
+	db.InitDB()
+
 	router := gin.Default()
-	router.GET("/employee", EmployeeController.GetEmployee)
+	router.GET("/employee", EmployeeController.GetEmployeeDB)
 	router.GET("/", EmployeeController.GetMain)
-	router.GET("/employee/:id", EmployeeController.GetEmployeeByID)
 	router.POST("/employee", EmployeeController.PostEmployee)
 	router.PUT("/employee", EmployeeController.PutEmployee)
 	router.DELETE("/employee", EmployeeController.DeleteEmployee)
